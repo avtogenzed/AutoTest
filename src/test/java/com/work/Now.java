07d2c5ef15dec9f87528217df68438c732a681e9
avtogenzed <70476169+avtogenzed@users.noreply.github.com>
2021-05-03 11:44:28 +0300

    Now merge spase (#9)
    * con
    
    * con

--- src/test/java/com/work/Now.java
+++ src/test/java/com/work/Now.java
@@ -3,5 +3,6 @@ package com.work;
 public class Now {
     private String n = "n";
     private String s = "s";
-    private String p = " p_der";
+    private String p = " p";
+    private String merge = "merge00;";
 }
